ccf1d9e3bacfd566ff7ba9e7ab9d614e3eeb2e18
Christopher Cid <iamcid17@gmail.com>
2020-04-02 22:25:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,66 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @pets = { :cats => [], :dogs => []}
+    @@all << self
+  end
+  
+  def say_species
+    @species = "I am a human."
+  end
+  
+  def cats
+    Cat.all.select do |cats| 
+      cats.owner == self
+    end
+  end
+  
+  def buy_cat(name)
+     @pets[:cats] << Cat.new(name, self)
+  end
+  
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+  
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name, self)
+  end
+  
+  def walk_dogs 
+    @pets[:dogs].each{|d| d.mood = "happy"}
+  end 
+  
+  def feed_cats
+    @pets[:cats].each{|c| c.mood = "happy"}
+  end
+  
+  def sell_pets
+    
+  end
+
+  
+  def list_pets
+    "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+  end
 end
